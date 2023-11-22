27445e855e19cd98731fe9a732d3e0ad52a4b546
Kristina Fokina <kristinalazareva1993@yandex.ru>
2023-11-22 13:45:52 +0300

    ПРоект готов

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cloneCardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+
+function removeCard(evt) {
+  evt.target.parentElement.remove();
+}
+
+function addInitialCards() {
+  initialCards.forEach((card) => {
+    const cardBlock = cloneCardTemplate.cloneNode(true);
+    const cardImage = cardBlock.querySelector(".card__image");
+    const cardTitle = cardBlock.querySelector(".card__title");
+    const deleteButton = cardBlock.querySelector(".card__delete-button");
+
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+    cardTitle.textContent = card.name;
+
+    deleteButton.addEventListener("click", removeCard);
+
+    placesList.append(cardBlock);
+  });
+}
+addInitialCards();
